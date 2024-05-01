886cf3cf8468def2e25fee425c32b67283253f82
Erick244 <erienrique03@gmail.com>
2024-05-01 16:32:43 -0300

    add attention message

--- src/app/decrypt/page.tsx
+++ src/app/decrypt/page.tsx
@@ -12,6 +12,10 @@ export default function Page() {
         <div className="w-full flex flex-col justify-center items-center gap-10">
             <H1>Decrypt</H1>
             <DecryptedFilesForm />
+            <p className="text-foreground text-sm bg-yellow-600/80 p-1 rounded">
+                This is just a test and why Crypto-File only supports small
+                files of less than 5Mb. This will change soon.
+            </p>
             <DecryptedFiles />
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
         <div className="w-full flex flex-col justify-center items-center gap-10">
             <H1>Encrypt</H1>
             <EncryptedFilesForm />
+            <p className="text-foreground text-sm bg-yellow-600/80 p-1 rounded">
+                This is just a test and why Crypto-File only supports small
+                files of less than 5Mb. This will change soon.
+            </p>
             <EncryptedFiles />
         </div>
     );
